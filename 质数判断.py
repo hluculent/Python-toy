82610a914831b81560c51c2699c9f7d4f7ea391f
Luculent <601994749@qq.com>
2016-03-24 23:10:41 +0800

    Update 质数判断.py

--- 质数判断.py
+++ 质数判断.py
@@ -19,20 +19,21 @@ ver.2
 用户界面友好并且可以多次输入
 """
 from math import sqrt
+
 def isprime(x):
     if x == 1:
         return False
-    k = int(sqrt(x))
+    k = int(sqrt(x)) #注意这边要强制整型
     for j in range(2, k+1):
-        if x%j == 0:
+        if x%j == 0: #只要x能被[2,sqrt(x))内的数字整除，x就不是素数
             return False
-    return True
+    return True #只有x不满足所有非素数的条件时，才是素数
 
-if __name__ == "__main__":
+if __name__ == "__main__":#不用调用main函数
     flag = 'y'
     while(flag == 'y'):
         num = input("Please input a number:")
-        for i in range(2,num):
+        for i in range(2,num+1):#输出num内（包括num）的所有非零素数
             if isprime(i) and num % i == 0:
                 print i,
         flag = raw_input("\nIf you want to input another number, \
